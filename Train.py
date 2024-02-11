5a01526b965079052bda997347c7d1ca58e064bc
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-02-11 21:14:40 +0300

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,13 +1,7 @@
-import re
-import sqlite3
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 
 tcdd_link = "https://ebilet.tcddtasimacilik.gov.tr/view/eybis/tnmGenel/tcddWebContent.jsf"
